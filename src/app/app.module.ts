f4a556a4ab9d2cb386910c44f60fb8e9fb6776cb
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-08-25 13:49:12 +0530

    Create a Service class for the ngrx data extending EntityCollectionServiceBase Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,12 @@ import { SinglePostComponent } from './posts/single-post/single-post.component';
 import { EditPostComponent } from './posts/edit-post/edit-post.component';
 import { AddPostComponent } from './posts/add-post/add-post.component';
 import { HomeComponent } from './home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entitly-metadata';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,14 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      logOnly: !isDevMode(),
+    }),
+    EffectsModule.forRoot([]),
+    EntityDataModule.forRoot(entityConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post..service';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent {
+
+export class PostsListComponent implements OnInit{
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getAll();
+  }
 
 }
