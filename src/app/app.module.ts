536f7e90dadce9aa4f81ed1bab11110dcb255e4c
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-08-25 12:57:35 +0530

    Setup new Project for Ngrx Data, installed all ngrx dependencies for ngrx data in Ngrx Angular App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    SinglePostComponent,
+    EditPostComponent,
+    AddPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
